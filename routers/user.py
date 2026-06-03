f0c6982fe397e5b6a8852baaaa2ed32db06ebcf5
Kay <80385994+twilightyear@users.noreply.github.com>
2026-06-03 16:38:58 +0900

    feat: add login user handler
    Added login user handler. It checks if it has wrong email or password

--- routers/user.py
+++ routers/user.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter, status, HTTPException
 from sqlalchemy import select
-from schema.request import UserSignUpRequest
+from schema.request import UserSignUpRequest, UserLoginRequest
 from database.db_connection import SessionFactory
 from models import User
-from auth.password import hash_password
+from auth.password import hash_password, verify_password
 from schema.response import UserSignUpResponse
 
 router = APIRouter(tags=["User"]) #API Wrapping group, function on User
@@ -39,3 +39,25 @@ def signup_user_handler(body: UserSignUpRequest):
 
         session.refresh(user) #Refresh db
         return user
+
+@router.post(
+    "/users/login",
+    status_code=status.HTTP_200_OK #On success
+)
+
+def login_user_handler(body: UserLoginRequest):
+    with SessionFactory() as session: #Create Session
+        stmt = select(User).where(User.email == body.email)
+        user = session.scalar(stmt)
+
+        if not user: #If user email is not found
+            raise HTTPException(
+                status_code = status.HTTP_401_UNAUTHORIZED,
+                detail = "Incorrect email or password"
+            )
+
+        if not verify_password(body.password, user.hashed_password): #If user password is wrong
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail= "Incorrect email or password"
+            )
